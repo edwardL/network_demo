fab13de71662b6809715e3076a5d2cf37e8a9cfb
U-USER7\Administrator <Administrator@USER7.(none)>
2016-11-17 10:35:02 +0800

    增加singlemanager unitest

--- unitest/heaptest.h
+++ unitest/heaptest.h
@@ -1,4 +1,5 @@
 #include "../frame/base/heap.h"
+#include "../frame/file/singlemanager.h"
 
 class HeapTest
 {
@@ -11,5 +12,21 @@ public:
 		h.Push(13);
 		h.Push(2);
 		h.PrintHeap();
+	}
+};
+
+class SingleManagerTest
+{
+public:
+	SingleManagerTest()
+	{
+		SingleManager m_single_manager;
+		m_single_manager.Init();
+		int* a = new int(1);
+		int* b = new int(2);
+		m_single_manager.Add(a);
+		m_single_manager.Add(b);
+		m_single_manager.Remove(a);
+		m_single_manager.Remove(b);
 	}
 };
